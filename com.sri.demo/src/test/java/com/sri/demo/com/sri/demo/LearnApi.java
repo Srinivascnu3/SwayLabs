65a526116d6b94c294280d45cca7df9ab9f2f73c
Admin <Admin@DESKTOP-D921UPH>
2025-12-19 19:04:14 +0530

    api commit

---
+++ com.sri.demo/src/test/java/com/sri/demo/com/sri/demo/LearnApi.java
@@ -0,0 +1,117 @@
+package com.sri.demo.com.sri.demo;
+
+import java.util.List;
+
+import org.json.JSONObject;
+import org.testng.Assert;
+import org.testng.annotations.AfterClass;
+import org.testng.annotations.BeforeClass;
+import org.testng.annotations.Test;
+
+import io.restassured.RestAssured;
+import io.restassured.http.Header;
+import io.restassured.http.Headers;
+import io.restassured.path.json.JsonPath;
+import io.restassured.response.Response;
+import io.restassured.specification.RequestSpecification;
+
+
+public class LearnApi {
+	String baseUrl;
+	String infyBase;
+	Response response;
+	
+  @Test
+	public void getApi() {
+		// response = RestAssured.get(baseUrl+"objects/4");
+		response = RestAssured.get(infyBase + "/api/v1/Activities");
+
+		String res = response.getBody().toString();
+		System.out.println(res);
+
+		String contentType = response.getContentType();
+		System.out.println("Content-Type value: " + contentType);
+
+		int status = response.getStatusCode();
+		System.out.println("status code:" + status);
+		Assert.assertEquals(200, status);
+		
+		// Get all the headers, return value is of type Headers.
+		Headers allHeaders = response.getHeaders();
+		// Headers class implements Iterable interface.
+		// Iterate over all the Headers using an advance for loop as shown in the code
+		// below
+		for (Header header : allHeaders) {
+			System.out.println("Key: " + header.getName() + " Value: " + header.getValue());
+		}
+		
+		String head= response.header("Server");
+		System.out.println(head);
+		
+		JsonPath path = response.jsonPath();
+		
+		List<Object> names = path.getList("");
+		for(Object name:names) {
+	          System.out.println(name);
+	      }    
+		//{id=1, title=Activity 1, dueDate=2025-12-19T04:20:55.4373722+00:00, completed=false}
+		
+		
+		
+		
+		
+	}
+
+	@Test(enabled = true)
+	public void postAPI() {
+		
+		RestAssured.baseURI = infyBase; // use base host and post to the explicit path
+		RequestSpecification req = RestAssured.given();
+
+		req.header("Content-Type", "application/json");
+
+		JSONObject reqParam = new JSONObject();
+
+		// Use proper JSON types: id as number, completed as boolean
+		reqParam.put("id", 33);
+		reqParam.put("title", "Srini Activity");
+		reqParam.put("dueDate", "2025-12-22T09:57:16.6203154+00:00");
+		reqParam.put("completed", false);
+		
+	
+		// Print the request body for debugging
+		System.out.println("Request JSON: " + reqParam.toString());
+		
+		req.body(reqParam.toString());
+		
+		// Post to the explicit resource path
+		 response = req.post("/api/v1/Activities");
+		
+		int statusCode = response.getStatusCode();
+		String statusLine = response.getStatusLine();
+		String respBody = response.getBody().asString();
+		System.out.println("Status code: " + statusCode);
+		System.out.println("Status line: " + statusLine);
+		System.out.println("Response body: " + respBody);
+		
+		// Helpful checks: 201 Created is common for successful POSTs
+		if (statusCode >= 200 && statusCode < 300) {
+			System.out.println("POST succeeded");
+		} else {
+			System.out.println("POST failed - check request body and API contract");
+		}
+		
+		
+
+	}
+  @BeforeClass
+  public void beforeClass() {
+	   baseUrl="https://api.restful-api.dev/";  
+	   infyBase = "https://fakerestapi.azurewebsites.net";
+  }
+
+  @AfterClass
+  public void afterClass() {
+  }
+
+}
--- com.sri.demo/src/test/java/com/sri/demo/com/sri/demo/NewTest.java
+++ com.sri.demo/src/test/java/com/sri/demo/com/sri/demo/NewTest.java
@@ -1,17 +1,20 @@
 package com.sri.demo.com.sri.demo;
 
 import java.time.Duration;
+import java.util.Arrays;
 import java.util.HashMap;
 import java.util.Map;
-import java.util.Arrays;
 
 import org.openqa.selenium.Alert;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.chrome.ChromeOptions;
 import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.Select;
 import org.openqa.selenium.support.ui.WebDriverWait;
+import org.testng.Assert;
 import org.testng.annotations.AfterClass;
 import org.testng.annotations.BeforeClass;
 import org.testng.annotations.Test;
@@ -23,6 +26,29 @@ public class NewTest {
 	public By username=By.id("user-name");
 	public By password=By.id("password");
 	public By loginButton=By.id("login-button");
+	
+	public By sortDropdown=By.className("product_sort_container");
+	//write by Locator for shopping cart link
+	public By shoppingCartLink=By.xpath("//a[@class='shopping_cart_link']");
+	
+	//checkout
+	public By checkoutButton=By.id("checkout");
+	
+	//first-name last-name postal-code
+	//write By locators for first-name,last-name and postal-code
+	public By firstName=By.id("first-name");
+	public By lastName=By.id("last-name");
+	public By postalCode=By.id("postal-code");
+	
+	public By continueButton=By.id("continue");
+	
+	By errorMessage=By.xpath("//h3[@data-test='error']");
+	
+	//summary_subtotal_label
+	By subtotalLabel=By.className("summary_subtotal_label");
+	
+	//finish
+	By finishButton=By.id("finish");
 	
   @Test
   public void login() {
@@ -47,6 +73,11 @@ public class NewTest {
 		String title = driver.getTitle();
 		System.out.println("Home Page Title is: " + title);
 		
+		//product_sort_container
+		WebElement filter = driver.findElement(sortDropdown);
+		Select s= new Select(filter);
+		s.selectByVisibleText("Price (low to high)");
+		
 		By thirdPrice =By.xpath("(//div[@class='inventory_item'])[3]//div[@class='inventory_item_price']");
 		String price =driver.findElement(thirdPrice).getText();
 		
@@ -54,7 +85,34 @@ public class NewTest {
 		//write xpath to find the 3rd product on the home page and click on it
 		By thirdProduct=By.xpath("(//div[@class='inventory_item'])[3]//button");
 		driver.findElement(thirdProduct).click();
-
+		//click on shopping cart link
+		driver.findElement(shoppingCartLink).click();
+		//click on checkout button
+		driver.findElement(checkoutButton).click();
+		
+		//error message validation
+		driver.findElement(continueButton).click();
+		
+		String errorText=driver.findElement(errorMessage).getText();
+		Assert.assertEquals(errorText, "Error: First Name is required");
+		
+		//enter first-name,last-name and postal-code
+		driver.findElement(firstName).sendKeys("John");
+		driver.findElement(lastName).sendKeys("Doe");
+		driver.findElement(postalCode).sendKeys("605752");
+		//click on continue button
+		driver.findElement(continueButton).click();
+		
+		String subtotal=driver.findElement(subtotalLabel).getText();
+		Assert.assertTrue(subtotal.contains(price));
+		
+		//click on finish button
+		driver.findElement(finishButton).click();
+		String finishMessage=driver.findElement(By.className("complete-header")).getText();
+		Assert.assertEquals(finishMessage, "Thank you for your order!");
+		
+		
+		
   }
   
   
@@ -66,6 +124,8 @@ public class NewTest {
 	  prefs.put("credentials_enable_service", false);
 	  prefs.put("profile.password_manager_enabled", false);
 	  options.setExperimentalOption("prefs", prefs);
+	  
+	  options.addArguments("--guest");
 	  // additional options to try and suppress Chrome UI infobars/password prompts
 	  options.addArguments("--disable-notifications");
 	  options.addArguments("--disable-infobars");
@@ -73,7 +133,12 @@ public class NewTest {
 	  // remove automation flag and disable automation extension
 	  options.setExperimentalOption("excludeSwitches", Arrays.asList("enable-automation"));
 	  options.setExperimentalOption("useAutomationExtension", false);
-	  
+	  /* 🔥 CRITICAL FLAGS 🔥 */
+	  options.addArguments("--disable-features=PasswordLeakDetection");
+	  options.addArguments("--disable-features=PasswordManagerOnboarding");
+	  options.addArguments("--disable-features=PasswordCheck");
+	  options.addArguments("--disable-features=SafeBrowsingEnhancedProtection");
+	  options.addArguments("--disable-save-password-bubble");
 	  
 	  
 	  driver = new ChromeDriver(options);
@@ -91,7 +156,7 @@ public class NewTest {
   @AfterClass
   public void afterMethod() {
 	  //close the browser
-	  //driver.quit();
+	  driver.quit();
   }
   
   }
